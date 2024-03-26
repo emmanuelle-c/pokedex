1104edc56ea9f80492e89f0f38d3448954d8fca3
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-03-26 11:04:56 +0100

    adaptative display

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,34 @@
 function PokemonCard() {
+    const pokemon = pokemonList[1];
+
+    if (pokemon.imgSrc !== undefined) {
     return (
     <figure>
-        <img src= "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"></img>
-        <figcaption>Bulbasaur</figcaption>
+        <img src={pokemon.imgSrc} >
+        </img>
+        <figcaption>{pokemon.name}</figcaption>
     </figure>
     )
+    }
+  else {
+    return (
+      <figure>
+      <p>???</p>
+      <figcaption>{pokemon.name}</figcaption>
+      </figure>
+    )
+  }  
 }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard;
