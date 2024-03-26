bc5276f1829baf9076c0da66ad7730bb40a70c04
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-03-26 20:42:23 +0100

    slider creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,53 @@
-import PokemonCard from "./components/PokemonCard"
+import PokemonCard from "./components/PokemonCard";
+import { useState} from "react";
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handleNextClick = () => {
+    setPokemonIndex(pokemonIndex +1);
+  };
+
+  const handlePreviousClick = () => {
+    setPokemonIndex(pokemonIndex -1);
+  };
+
+  if (pokemonIndex )
   return (
     <div>
-      <PokemonCard pokemon ={pokemonList[0]}/>
+      <PokemonCard pokemon ={pokemonList[pokemonIndex]}/> 
+      <button onClick={handlePreviousClick}>Précédent</button>
+      <button onClick={handleNextClick}>Suivant</button>
     </div>
   );
 }
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
 
 export default App;
